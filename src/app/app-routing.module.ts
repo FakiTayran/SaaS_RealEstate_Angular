dd3d01919be2f91e50df14f9d5f3ee76e86c53e5
fakitayran <aridurufakitayran@gmail.com>
2024-10-09 01:23:57 +0100

    authguard error fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,30 @@ import { AddEstateComponent } from './components/addEstate/addEstate.component';
 import { FullComponent } from './layouts/full/full.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { EstateListComponent } from './components/estateList/estateList.component'; // Import EstateListComponent
+import { EstateListComponent } from './components/estateList/estateList.component';
 import { AuthGuard } from './guards/auth.guard';
 import { EditEstateComponent } from './components/editEstate/editEstate.component';
 import { AddEstateAgentComponent } from './components/addEstateAgent/addEstateAgent.component';
 import { EstateAgentListComponent } from './components/estateAgentList/estateAgentList.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }, // Login rotası
-  { path: 'register', component: RegisterComponent }, // Register rotası
+  { path: 'login', component: LoginComponent }, // Giriş rotası
+  { path: 'register', component: RegisterComponent }, // Kayıt rotası
   {
     path: '',
     component: FullComponent,
-    canActivate: [AuthGuard], // FullComponent için AuthGuard ekleyin
+    canActivate: [AuthGuard], // FullComponent için AuthGuard
     children: [
-      { path: '', redirectTo: '/estateList', pathMatch: 'full' },
-      { path: 'home', redirectTo: '/estateList', pathMatch: 'full' },
+      { path: '', redirectTo: 'estateList', pathMatch: 'full' }, // Ana sayfa yönlendirmesi
+      { path: 'home', redirectTo: 'estateList', pathMatch: 'full' }, // Home yönlendirmesi
       { path: 'addEstate', component: AddEstateComponent },
       { path: 'editEstate', component: EditEstateComponent },
-      { path: 'estateList', component: EstateListComponent }, 
-      { path: 'addAgent', component: AddEstateAgentComponent }, 
-      { path: 'agentList', component: EstateAgentListComponent }, 
-
+      { path: 'estateList', component: EstateListComponent },
+      { path: 'addAgent', component: AddEstateAgentComponent },
+      { path: 'agentList', component: EstateAgentListComponent },
     ]
   },
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'login', pathMatch: 'full' } // Bilinmeyen yollar için login sayfasına yönlendirme
 ];
 
 @NgModule({
